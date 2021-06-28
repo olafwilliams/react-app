0931461cc2806e89a51d5bc6e7d007c75707673d
Olaf Williams <owilliams10@gmail.com>
2021-06-28 21:40:54 +0200

    wf

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn jou bliksem
+                    Learn React
                 </a>
             </header>
         </div>
