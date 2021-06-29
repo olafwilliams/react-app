e3df64fee39f1bea9a76c815a82ba1b5e1e2bbdc
Olaf Williams <owilliams10@gmail.com>
2021-06-29 19:03:26 +0200

    Added things

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload biiiiitch.
+                    Edit <code>src/App.js</code> and save to reload Naaiers.
                 </p>
                 <a
                     className="App-link"
