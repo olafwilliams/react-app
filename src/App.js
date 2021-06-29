154ce14fcf9bb57d0d7a8bd4172f483d757433ed
Olaf Williams <owilliams10@gmail.com>
2021-06-29 20:07:52 +0200

    dafuq

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload Naaierz.
+                    Edit <code>src/App.js</code> and save to reload Naaierzaa.
                 </p>
                 <a
                     className="App-link"
